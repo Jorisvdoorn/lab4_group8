f2826af4439c314a236dfb9848a6316ca29570bb
Joris van Doorn <joris.v.doorn@gmail.com>
2019-09-23 11:48:34 +0200

    started with the RC oo and the function

---
+++ R/linreg.R
@@ -0,0 +1,26 @@
+linreg<-setRefClass("linreg",
+                    fields = list(
+                      betahat = "numeric", # regressions coefficients
+                      yhat = "numeric", # fitted values
+                      reshat = "numeric", # residuals
+                      df = "numeric", # degrees of freedom
+                      resvar = "numeric", # residual variance
+                      betavar = "numeric", # variance of the regression coefficients
+                      betat = "numeric", # t-values for each coefficient
+                                  ),
+                    methods = list(
+                      print = function(x) {something}, # does something
+                      plot = function(x) {somethingelse}, # does something else
+                      resid = function(x) {somethingelse}, # does something else
+                      pred = function(x) {somethingelse}, # does something else
+                      coef = function(x) {somethingelse}, # does something else
+                      summary = function(x) {somethingelse}, # does something else
+                    )
+)
+
+
+formula<-
+
+linreg<-function(formula, data){
+    
+}
